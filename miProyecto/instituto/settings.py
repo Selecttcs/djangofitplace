7f00ae64f87cdb2bc9c01e2429013959adc25535
unknown <CETECOM@AOAWSB03LC0910.duoc.acad>
2025-05-03 11:42:26 -0400

    db

--- miProyecto/instituto/settings.py
+++ miProyecto/instituto/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'instituto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/XEPDB1',  # o 'XE'
+        'USER': 'SYSTEM',
+        'PASSWORD': 'SYSTEM',
     }
 }
 
